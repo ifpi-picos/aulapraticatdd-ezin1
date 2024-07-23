e00cbdef21fbbc888ab2ab836355a8ca787b47e9
ezin1 <93697880+ezin1@users.noreply.github.com>
2024-07-23 12:05:18 -0300

    Second commit

--- money.java
+++ money.java
@@ -1,39 +1,30 @@
 class Money  {
-   protected int amount;
-   
-   public boolean equals(Object object)  {
-      Money money = (Money) object;
-      return amount == money.amount;
-   }   
-}
+	protected int amount;
+	protected String currency;
 
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 
-class Dollar {
-   private int amount;
-   Dollar(int amount) {
-      this.amount = amount;
-   }
-   Dollar times(int multiplier) {
-      return new Dollar(amount * multiplier);
-   }
+	static Money dollar(int amount)  {
+		return new Money(amount, "USD");
+	}
 
-   public boolean equals(Object object)  {
-   Dollar dollar = (Dollar) object;
-   return amount == dollar.amount;
-}
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
 
-}
+	public boolean equals(Object object)  {
+		Money money = (Money) object;
+		return amount == money.amount && currency().equals(money.currency());
+	}
 
-class Franc {   
-   private int amount;					
-   Franc(int amount) {      
-      this.amount= amount;
-    }					
-    Franc times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
-    }   
-    public boolean equals(Object object) {					
-       Franc franc = (Franc) object;      
-       return amount == franc.amount;					
-     }					
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+
+	String currency() {
+		return currency;
+	}
 }
